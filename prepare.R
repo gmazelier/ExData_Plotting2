2d8ab334c9e97cf424b61e8ad98043affe341893
Gaylord Mazelier <gaylord@mazelier.fr>
2014-12-16 15:11:40 +0100

    Download and unzip data files if required.

---
+++ prepare.R
@@ -0,0 +1,20 @@
+## This utility script will fetch the required data.
+
+fetchData <- function(src, dst) {
+    res <- length(Filter(file.exists, dst))
+    if (length(dst) != res) {
+        target <- "data.zip"
+        message(sprintf("Downloading data from '%s'", src))
+        download.file(src, target, method = "curl", quiet = TRUE)
+        message(sprintf("Extracting to '%s'\n", dst))
+        unzip(target)
+    } else {
+        message(sprintf("File '%s' already exists, skipping download\n", dst))
+    }
+}
+
+getData <- function() {
+    src <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+    dst <- c("summarySCC_PM25.rds", "Source_Classification_Code.rds")
+    fetchData(src, dst)
+}
